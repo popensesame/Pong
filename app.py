a69b85b9abf2d8c236390f36a8b17e57a5d9a177
Matt Geiger <violincounter@gmail.com>
2014-04-26 19:51:15 -0400

    create pong and game class, add app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+__author__ = 'Matt Geiger'
+
+import pong
+
+if __name__ == '__main__':
+    app = pong.Pong()
+    app.run()
---
+++ game.py
@@ -0,0 +1,41 @@
+__author__ = 'Matt Geiger'
+
+import paddle as _paddle
+import ball as _ball
+import pygame as pg
+
+
+class Game:
+    def __init__(self, screen, screen_rect, pause=True):
+        self.pause = pause
+        self.screen_rect = screen_rect
+        self.screen = screen
+        self.ball = _ball.Ball(self.screen_rect)
+        self.player = _paddle.Paddle(screen_rect, 785, 275)
+        self.computer = _paddle.Paddle(screen_rect, 0, 275)
+        self.done = False
+
+    def render(self):
+        self.screen.fill(0)
+        self.ball.render(self.screen)
+        self.player.render(self.screen)
+        self.computer.render(self.screen)
+
+    def get_event(self, event, keys):
+        if event.type == pg.QUIT:
+            pg.quit()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                self.done = True
+            if event.key == pg.K_UP:
+                self.player.move(-6)
+            if event.key == pg.K_DOWN:
+                self.player.move(6)
+
+    def update(self):
+        self.ball.update(self.computer.rect, self.player.rect)
+
+
+
+
+
